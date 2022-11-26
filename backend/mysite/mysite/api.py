9761f29a9a4fbdbf7703f22baa960b4267d1d79b
wjxchn <1822630963@qq.com>
2022-11-26 21:04:06 +0800

    更新了15个接口

--- backend/mysite/mysite/api.py
+++ backend/mysite/mysite/api.py
@@ -15,3 +15,59 @@ def api_test(request):
     else:
         ret_getdict = {'code': 400, 'msg': "查询失败"}
         return JsonResponse(ret_getdict)
+
+def api_login(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_register(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_getuserinfo(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_changeuserinfo(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_addclass(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_getclassinfo(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_changeclassinfo(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_deleteclass(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_arrangeclass(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_addclassroom(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_getclassroominfo(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_modifyclassroominfo(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_getarrangeclasshistory(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
+
+def api_selectcourse(request):
+    ret_getdict = {'code': 400, 'msg': "查询失败"}
+    return JsonResponse(ret_getdict)
